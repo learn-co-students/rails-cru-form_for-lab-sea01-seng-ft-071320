a9620ba536059b86e85038ac76a5fa785ded8884
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-08-06 11:31:23 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+Artist.create(name: "Adele", bio:"Adele Laurie Blue Adkins MBE born 5 May 1988 is an English singer-songwriter.")
+Artist.create(name:"Lady Gaga", bio:"Stefani Joanne Angelina Germanotta born March 28, 1986, known professionally as Lady Gaga, is an American singer, songwriter, record producer, actress, and businesswoman.")
+Artist.create(name:"Madoona", bio:"Stefani Joanne Angelina Germanotta born March 28, 1986, known professionally as Lady Gaga, is an American singer, songwriter, record producer, actress, and businesswoman.")
+Artist.create(name:"Drake", bio:"Stefani Joanne Angelina Germanotta born March 28, 1986, known professionally as Lady Gaga, is an American singer, songwriter, record producer, actress, and businesswoman.")
 
-adel = Artist.create(name: "Adele", bio:"Adele Laurie Blue Adkins MBE born 5 May 1988 is an English singer-songwriter.")
-gaga  =Artist.create(name:"Lady Gaga", bio:"Stefani Joanne Angelina Germanotta born March 28, 1986, known professionally as Lady Gaga, is an American singer, songwriter, record producer, actress, and businesswoman.")
+Genre.create(name: "Contemporary R&B")
+Genre.create(name:"Pop")
+Genre.create(name:"Rock")
+Genre.create(name:"Jazz")
 
-rnb = Genre.create(name: "Contemporary R&B")
-pop = Genre.create(name:"Pop")
+Song.create(name:"Ko", artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id)
+Song.create(name:"j", artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id)
+Song.create(name:"if", artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id)
+Song.create(name:"sd", artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id)
+Song.create(name:"bd", artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id)
 
-hello = Song.create(name:"Hello", artist_id: adel.id, genre_id: rnb.id)
-telephone = Song.create(name:"Telephone", artist_id: gaga.id, genre_id: pop.id)
